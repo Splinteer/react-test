e0aeef91de455c5663f23ae3b14f2feb79ccecdf
Marvin Sanchez <sanchezmarvin38@gmail.com>
2017-01-31 22:16:11 +0100

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,8 @@ const PATHS = {
 }
 const TARGET = process.env.npm_lifecycle_event
 
+process.env.BABEL_ENV = TARGET
+
 const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
@@ -26,12 +28,22 @@ const common = {
     filename: 'bundle.js'
   },
   module: {
+
     loaders: [
       {
         // Test expects a RegExp! Note the slashes!
         test: /\.css$/,
         loaders: ['style', 'css'],
         // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015', 'survivejs-kanban']
+        },
         include: PATHS.app
       },
       // Set up jsx. This accepts js too thanks to RegExp
